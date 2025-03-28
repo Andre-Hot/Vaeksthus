4d0eb6bd1c2e8e4e286675826729607b68415d0b
Søren T <sotr0001@stud.kea.dk>
2025-03-28 10:47:59 +0100

    ny kode

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-kjshfkhsdjkhfds
+gpiozero import PWMLED
+from time import sleep
+
+led = PWMLED(20)
+
+led.value = 1   
+sleep(1)
+#led.value = 0.5  
+#sleep(1)
+#led.value = 0   
+#sleep(1)
+
+try:
+  while True:
+    for duty_cycle in range(0, 100, 1):
+      led.value = duty_cycle/100.0
+      sleep(0.05)
+
+    for duty_cycle in range(100, 0, -1):
+      led.value = duty_cycle/100.0
+      sleep(0.05)
+      
+except KeyboardInterrupt:
+  print("Stopping program, turning off the LED")
+  led.value = 0
+  pass
